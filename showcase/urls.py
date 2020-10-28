6fb35d988a941b8fc466590b05b887e67a6302ef
ibukamshindi <ibukamshindi@gmail.com>
2020-10-28 19:16:55 +0300

    add more views and index.html

--- showcase/urls.py
+++ showcase/urls.py
@@ -1,12 +1,15 @@
 from showcase import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 from django.conf.urls import url,include
 from django.conf import settings
 
 
 urlpatterns=[
     url(r'^$', views.register, name='register'),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'^home/$',views.home, name='home'),
 ]
 
 if settings.DEBUG:
