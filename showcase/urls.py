9844cb71e606785702883b8ce19e064377196811
ibukamshindi <ibukamshindi@gmail.com>
2020-10-28 12:39:04 +0300

    create views home add urls

--- showcase/urls.py
+++ showcase/urls.py
@@ -1,9 +1,13 @@
+from showcase import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url,include
+from django.conf import settings
 
 
 urlpatterns=[
-#..............
+    url(r'^$', views.register, name='register'),
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
