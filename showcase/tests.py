edf936500c7d12f8d3592405f239fa9b38bd25f4
ibukamshindi <ibukamshindi@gmail.com>
2020-10-29 02:27:18 +0300

    create test for profile behaviors

--- showcase/tests.py
+++ showcase/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image,Projects,Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.profile=Profile(profile_photo='this is me',bio='execeptional')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile,Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles=Profile.objects.all()
+        self.assertTrue(len(profiles)>0)  
