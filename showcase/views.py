505c79637f2c5c2eb62b2012a074aa8d4825936a
ibukamshindi <ibukamshindi@gmail.com>
2020-10-28 22:36:32 +0300

    add forms.py templates adjust views

--- showcase/views.py
+++ showcase/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import logout
 from django.contrib.auth.decorators import login_required
-# from .forms import *
+from .forms import *
 from django.http import Http404
 
 # Create your views here.
@@ -29,3 +29,59 @@ def project(request, project_id):
     except Projects.DoesNotExist:
         raise Http404()
     return render(request, "project.html", locals())
+
+@login_required(login_url='/accounts/login')
+def profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UploadForm()
+        my_projects = Projects.objects.filter(owner=current_user)
+        my_profile = Profile.objects.get(user_id=current_user)
+    return render(request, 'profile.html', locals())
+
+@login_required(login_url='/accounts/login')
+def upload_form(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.uploaded_by = current_user
+            image.save()
+            return redirect('home')
+    else:
+        form = UploadForm()
+    return render(request, 'post.html', {'uploadform': form})
+
+
+@login_required(login_url='/accounts/login')
+def edit_prof(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            lol = form.save(commit=False)
+            lol.uploaded_by = current_user
+            lol.save()
+            return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile_edit.html', {'profileform': form})
+
+
+@login_required(login_url='/accounts/login')
+def search(request):
+    all_projects = Projects.objects.all()
+    parameter = request.GET.get("project")
+    result = Projects.objects.filter(project_name__icontains=parameter)
+    return render(request, 'search.html', locals())
+
+
+@login_required(login_url='/accounts/login')
+def logout_view(request):
+    logout(request)
